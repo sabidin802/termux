60193aeb261875956a56ecba90aa287948ad6345
sabidin802 <82277132+sabidin802@users.noreply.github.com>
2021-04-16 11:09:37 +0700

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,9 +1,9 @@
 pkg install -y expect wget qemu-utils qemu-common qemu-system-x86_64-headless
 mkdir -p alpine
 cd alpine
-wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/ssh2qemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/ssh2qemu.sh
 chmod +x ./ssh2qemu.sh
-wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/startqemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/startqemu.sh
 chmod +x ./startqemu.sh
-wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/installqemu.expect
+wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/installqemu.expect
 expect -f installqemu.expect

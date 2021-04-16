dc1dd0df854a7acfe7e40a46a6f4194bdd382279
sabidin802 <82277132+sabidin802@users.noreply.github.com>
2021-04-16 11:04:11 +0700

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -15,11 +15,11 @@ sudo apt-get -t buster-backports install -y qemu qemu-system-common qemu-system-
 
 mkdir -p alpine
 cd alpine
-wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/ssh2qemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/ssh2qemu.sh
 chmod +x ./ssh2qemu.sh
-wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/startqemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/startqemu.sh
 chmod +x ./startqemu.sh
 sed -i "s:\$PREFIX/share/qemu/edk2-x86_64-code.fd:/usr/share/qemu/OVMF.fd:g" ./startqemu.sh
-wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/installqemu.expect
+wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/installqemu.expect
 sed -i "s:\$env(PREFIX)/share/qemu/edk2-x86_64-code.fd:/usr/share/qemu/OVMF.fd:g" ./installqemu.expect
 expect -f installqemu.expect

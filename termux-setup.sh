0a87905f04a22203f556512356103ae0771172f7
sabidin802 <82277132+sabidin802@users.noreply.github.com>
2021-04-16 11:20:48 +0700

    Delete termux-setup.sh

--- termux-setup.sh
+++
@@ -1,9 +0,0 @@
-pkg install -y expect wget qemu-utils qemu-common qemu-system-x86_64-headless
-mkdir -p alpine
-cd alpine
-wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/ssh2qemu.sh
-chmod +x ./ssh2qemu.sh
-wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/startqemu.sh
-chmod +x ./startqemu.sh
-wget -q -c -t0 https://raw.githubusercontent.com/sabidin802/termux/main/installqemu.expect
-expect -f installqemu.expect

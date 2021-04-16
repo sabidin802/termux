4777664cb34e60a81b47ea4b4f58366d6303fda4
sabidin802 <82277132+sabidin802@users.noreply.github.com>
2021-04-16 10:44:49 +0700

    Add files via upload

---
+++ termux-setup.sh
@@ -0,0 +1,9 @@
+pkg install -y expect wget qemu-utils qemu-common qemu-system-x86_64-headless
+mkdir -p alpine
+cd alpine
+wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/ssh2qemu.sh
+chmod +x ./ssh2qemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/startqemu.sh
+chmod +x ./startqemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/egandro/docker-qemu-arm/master/installqemu.expect
+expect -f installqemu.expect
